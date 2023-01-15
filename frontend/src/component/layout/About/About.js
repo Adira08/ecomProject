8077b7d804303936780df120e509ff10e301a605
Aditya Raj <adiraj8696@gmail.com>
2023-01-16 01:42:11 +0530

    first commit

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitGitHub = () => {
+    window.location = "https://github.com/Adira08";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/djbcyqihl/image/upload/v1673804019/avatars/nlrxbdvrkbmsssz8cgzm.jpg"
+              alt="Founder"
+            />
+            <Typography>Aditya Raj</Typography>
+            <Button onClick={visitGitHub} color="primary">
+              Visit GitHub
+            </Button>
+            <span>
+              To view the code for this project you can visit my GitHub account by clicking in above button
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
